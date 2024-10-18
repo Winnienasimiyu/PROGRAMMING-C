e74a77202a183ce01f8cae13d0a19f9cea5d0e89
Winnienasimiyu <winnienasimiyu756@gmail.com>
2024-10-18 11:32:03 +0300

    Add files via upload

---
+++ myloans.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+int age;
+float income;
+printf("Enteryourage:");
+scanf("%d",&age);
+printf("Enter your monthly income:");
+scanf("%f",&income);
+if(age>21000){
+printf("CONGRATULATIONS YOU QUALIFY FOR THE LOAN.\n");
+}
+if(income>=21000){
+printf("CONGRATULATIONS YOU QUALIFY FOR THE LOAN.\n");
+}else{
+printf("UNFORTUNATELY WE ARE UNABLE TO OFFER YOU A LOAN AT THIS TIME.\n");
+}
+return 0;
+}
