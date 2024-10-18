b5d073d20f839770139afc00b9fb9d1a029cdcc9
Winnienasimiyu <winnienasimiyu756@gmail.com>
2024-10-18 11:36:23 +0300

    Add files via upload

---
+++ one dimension.c
@@ -0,0 +1,10 @@
+//array in c one dimension 
+#include<stdio.h>
+int main(){
+int i;
+int marks[6]={20,40,60,80,30,10};
+for(i=0;i<6;i++){
+printf("marks [%d] = %d \n",i,marks[i]);
+}
+    return 0;
+}
