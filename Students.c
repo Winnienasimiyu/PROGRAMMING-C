5d89ae6bd70e387727a68be6138eb40e1266034f
Winnienasimiyu <winnienasimiyu756@gmail.com>
2024-10-31 19:05:02 +0300

    Add files via upload

---
+++ Students.c
@@ -0,0 +1,31 @@
+// c program to read name and marks of students 
+#include<stdlib.h>
+
+int main() {
+	char name[5];
+	float marks;
+	int i;
+	int n;
+	FILE *fptr;
+	
+	fptr = fopen("C:\\massvc\\Edit.txt","w");
+	
+	printf("Enter n(Number of students): ");
+	scanf("%d", &n);
+	
+	for(i=0;i<n;i++){
+		printf("Enter the name of student%d: ", i+1);
+		scanf(" %[^\n]", &name);
+		
+		printf("Enter student's marks%d: ", i+1);
+		scanf("%f", &marks);
+		
+		fprintf(fptr, "%s\n%.2f\n", name, marks);
+	}
+	printf("Data upload successful\n");
+	
+	fclose(fptr);
+	
+	return 0;
+}
+
